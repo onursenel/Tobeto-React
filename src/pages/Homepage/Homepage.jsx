2a745c7cb2092fdbb75ccf15ffea2cfec664e6c5
Onur Şenel <onrsenel@gmail.com>
2023-12-26 02:51:42 +0300

    async-await-promise

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -7,19 +7,38 @@ export default function Homepage() {
     return new Promise((resolve,reject)=>{
       //async işlem
       setTimeout(()=>{
-        resolve("veri örneği"); // işlemin başarılı olması
-        //reject("istek başarısız");// işlemin başarısız olması
+        //resolve("veri örneği"); // işlemin başarılı olması
+        reject("istek başarısız");// işlemin başarısız olması
       },3000)
     });
   }
 
   useEffect(() => {
-    myAsyncFunciton()
-        .then(response => {console.log("bir cevap geldi ", response)})
-        .catch(err => {console.log("bir hata geldi", err)})
-        .finally(() => {console.log("Async işlem sonlandı")});
+    //thenCatchFinally();
+    asyncAwait();
     
-  },[])
+  },[]);
+
+  const asyncAwait =async ()=> {
+    try{
+      let response = await myAsyncFunciton();
+      console.log(response);
+
+    }catch(e)
+    {
+      console.log(e)
+    }
+ 
+  }
+
+  const thenCatchFinally = () => {
+
+    myAsyncFunciton()
+    .then(response => {console.log("bir cevap geldi ", response)})
+    .catch(err => {console.log("bir hata geldi", err)})
+    .finally(() => {console.log("Async işlem sonlandı")});
+
+  }
 
   //JS ASYNC
   //Built-in Promise
