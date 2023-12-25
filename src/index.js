fc59bb0ebae1507d25b92dc5fecd0199bc59a12c
Onur Şenel <onrsenel@gmail.com>
2023-12-26 02:47:51 +0300

    async-await-promise yapıları

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+//export edilen App function default olarak export edildiğinden dolayı {} içerisine almadık
+//export edilirken default yazılmaz ise burada import edilirken {} arasında export edilen yazılmalıdır.
+//Default olarak sadece 1 alan export edilebilir. exportlara birden fazla default tanımlanamaz.
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,41 @@
+import React, { useDebugValue, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Homepage() {
+
+  const myAsyncFunciton = () => {
+    return new Promise((resolve,reject)=>{
+      //async işlem
+      setTimeout(()=>{
+        resolve("veri örneği"); // işlemin başarılı olması
+        //reject("istek başarısız");// işlemin başarısız olması
+      },3000)
+    });
+  }
+
+  useEffect(() => {
+    myAsyncFunciton()
+        .then(response => {console.log("bir cevap geldi ", response)})
+        .catch(err => {console.log("bir hata geldi", err)})
+        .finally(() => {console.log("Async işlem sonlandı")});
+    
+  },[])
+
+  //JS ASYNC
+  //Built-in Promise
+  
+
+
+  return (
+    <div>
+      Homepage sayfası
+      <br/>
+
+      <a href="/products">Ürünler sayfası</a>
+
+      <br />
+
+      <Link to={"/products"}>Ürünler sayfası</Link>
+      </div>
+  )
+}
